907f9b0648749d12ea874d8035e91ad8b8d844c7
Mauri Miguel <mmiguelcj@gmail.com>
2018-02-09 13:25:38 -0200

    api github service

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubService {
 
-  constructor() { }
+  constructor(private _http: Http) {
+    console.log('Github Service Working!');
+   }
 
+  getUsers(data) {
+    return this._http.get('https://api.github.com/search/users?q=tom+repos:%3E42')
+    .map(res => res.json());
+  }
 }
